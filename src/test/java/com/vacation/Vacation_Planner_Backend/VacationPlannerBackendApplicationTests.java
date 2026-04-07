8f30a8cfaa636f4cdca6c24d44d4248e5d30540b
S1notik <semn.osipov.2007@bk.ru>
2026-04-07 19:15:08 +0300

    feat: create base of project

---
+++ src/test/java/com/vacation/Vacation_Planner_Backend/VacationPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacation.Vacation_Planner_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPlannerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
